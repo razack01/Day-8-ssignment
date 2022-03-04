0a9eb84b4a9ad169be1498fad71a5434bf7d5656
Abdul Razack <75486001+razack01@users.noreply.github.com>
2022-03-04 19:06:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+class Circle{
+	constructor(radius,color){
+	this.radius=radius
+	this.color=color
+}
+	
+	getradius(){
+	return this.radius
+
+}
+	getcolor(){
+  	return this.color
+
+}
+getArea(){
+  	return this.radius
+
+}
+getCircumference(){
+  	return this.radius
+
+}
+
+}
+
+
+var a= new Circle(1.0,"red")
+ console.log(a)
+ console.log(a.getArea())
